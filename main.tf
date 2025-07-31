49f723200ee5a952cb12aefa91335d6ac5557374
Ibrahimbaig1997 <ibrahimbaig12345@gmail.com>
2025-07-31 09:08:22 +0000

    added st

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "Day25-rg"
   location = "Canada Central"
 }
+
+resource "azurerm_storage_account" "st" {
+    name = "ibrstraaday25"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    account_tier             = "Standard"
+    account_replication_type = "GRS"
+}
